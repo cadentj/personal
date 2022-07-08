4754aa6b279974b6bef598cc05c8b72cf0a3bdfe
Caden <caden.juang@gmail.com>
2022-07-08 17:19:20 -0500

    Updates
    - Text color change

--- src/components/HorizontalTiles.js
+++ src/components/HorizontalTiles.js
@@ -16,7 +16,8 @@ import '../styles/tiles.css';
 const dummy = new Vector3()
 let xDim;
 const titles = ["MAV APP", "HOUSING MODEL", "EMAIL BOT", "THUNDER DASH", "CODE CLUB", "AI CLUB"]
-const colors = ["#DE4C3F", "#DE4C3F", "#DE4C3F", "#DE4C3F", "#DE4C3F", "#DE4C3F"]
+const colors = ["#DE4C3F", "#ECE8DE", "#DE4C3F", "#DE4C3F", "#DE4C3F", "#DE4C3F"]
+const textColors = ["#FFF1CE", "#55729C", "#FFF1CE", "#FFF1CE", "#FFF1CE", "#FFF1CE"]
 
 function Item({ index, position, scale, c = new THREE.Color(), ...props }) {
     const ref = useRef()
@@ -28,13 +29,16 @@ function Item({ index, position, scale, c = new THREE.Color(), ...props }) {
     const click = () => {
         state.clicked = index === clicked ? null : index
         props.setTitles(titles[index])
-        props.setColors(colors[index])
+        props.setColor(colors[index])
+        props.setTextColor(textColors[index])
     }
     const over = () => hover(true)
     const out = () => hover(false)
 
     function unclick() {
         state.clicked = null
+        props.setTitles("")
+        props.setColor("#141414")
     }
 
     const [centered, center] = useState(false)
@@ -54,8 +58,8 @@ function Item({ index, position, scale, c = new THREE.Color(), ...props }) {
 
         if (clicked === index) ref.current.position.x = damp(ref.current.position.x, difference, 6, delta)
         if (scroll.delta > 0.001) unclick()
-        if (clicked === null) props.setTitles("")
-        if (clicked === null) props.setColors("#141414")
+
+
 
         ref.current.material.grayscale = damp(ref.current.material.grayscale, hovered || clicked === index ? 0 : Math.max(0, 1 - y), 6, delta)
         ref.current.material.color.lerp(c.set(hovered || clicked === index ? 'white' : '#aaa'), hovered ? 0.3 : 0.1)
@@ -63,17 +67,16 @@ function Item({ index, position, scale, c = new THREE.Color(), ...props }) {
     return <Image ref={ref} {...props} position={position} scale={scale} height={2} onClick={click} onPointerOver={over} onPointerOut={out} />
 }
 
-function Items({ w = 0.7, gap = 0.15, setTitles, setColors }) {
+function Items({ w = 0.7, gap = 0.15, setTitles, setColor, setTextColor }) {
     const { urls } = useSnapshot(state)
     const { width } = useThree((state) => state.viewport)
     const xW = w + gap
     xDim = xW * (urls.length - 1)
-    console.log(xDim)
     return (
         <ScrollControls horizontal damping={10} pages={(width - xW + urls.length * xW) / width}>
             <Minimap />
             <Scroll>
-                {urls.map((url, i) => <Item key={i} index={i} position={[xW * i, 0, 0]} scale={[w, 4, 1]} url={url} setTitles={setTitles} setColors={setColors} />) /* prettier-ignore */}
+                {urls.map((url, i) => <Item key={i} index={i} position={[xW * i, 0, 0]} scale={[w, 4, 1]} url={url} setTitles={setTitles} setColor={setColor} setTextColor={setTextColor} />) /* prettier-ignore */}
             </Scroll>
         </ScrollControls>
     )
@@ -82,6 +85,8 @@ function Items({ w = 0.7, gap = 0.15, setTitles, setColors }) {
 function Screen(props) {
     const [titleTop, setTop] = useState("");
     const [titleBottom, setBottom] = useState("");
+
+    const [textColor, setTextColor] = useState("");
 
     function setTitles(title) {
         const split = title.split(' ')
@@ -97,7 +102,7 @@ function Screen(props) {
             <Text
                 scale={[10, 10, 10]}
                 position={[0, 1.5, 0]}
-                color="white" // default
+                color={textColor} // default
                 font={Montserrat}
                 letterSpacing={0.3}
             >
@@ -106,13 +111,13 @@ function Screen(props) {
             <Text
                 scale={[10, 10, 10]}
                 position={[0, -1.5, 0]}
-                color="white" // default
+                color={textColor} // default
                 font={Montserrat}
                 letterSpacing={0.3}
             >
                 {titleBottom}
             </Text>
-            <Items setTitles={setTitles} setColors={props.setColors} />
+            <Items setTitles={setTitles} setColor={props.setColor} setTextColor={setTextColor} />
         </>
     )
 }
@@ -120,14 +125,10 @@ function Screen(props) {
 export default function HorizontalTiles() {
     const [color, setColor] = useState("");
 
-    function setColors(color) {
-        setColor(color)
-    }
-
     return (
         <Box sx={{ height: '100vh', width: '100%', position: 'fixed', backgroundColor: color }} className="background">
             <Canvas gl={{ antialias: false }} dpr={[1, 1.5]} onPointerMissed={() => (state.clicked = null)}>
-                <Screen setColors={setColors} />
+                <Screen setColor={setColor} />
             </Canvas>
         </Box>
     )
